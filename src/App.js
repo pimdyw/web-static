5bf2ac64ee93f3c0b11f0d46195aed0c41ec9d83
Pimdyw <125757525+pimdyw@users.noreply.github.com>
2024-07-31 17:12:51 +0700

    Delete img

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   const value = 'Nong PIM';
   return (
     <div>
+      <h1>Today's Diary</h1>
       Hello {value}
-      <img src="https://media.discordapp.net/attachments/725983079675461725/1216755952548839555/IMG_20240130_154639_754.webp?ex=66aaf409&is=66a9a289&hm=289edce02710372532097c0bc529d92f4c28be348b2b0947a5a14713cbe459d9&=&format=webp&width=412&height=733" />
     </div>
   );
 }
