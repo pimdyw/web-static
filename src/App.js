035efbb709cb70f8b5424e694a0c82dce49a3a77
Pimdyw <125757525+pimdyw@users.noreply.github.com>
2024-07-31 16:54:01 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function App() {
   const value = 'Nong PIM';
-  return <div>Hello {value}</div>;
+  return (
+    <div>
+      Hello {value}
+      <img src="https://media.discordapp.net/attachments/725983079675461725/1216755952548839555/IMG_20240130_154639_754.webp?ex=66aaf409&is=66a9a289&hm=289edce02710372532097c0bc529d92f4c28be348b2b0947a5a14713cbe459d9&=&format=webp&width=412&height=733" />
+    </div>
+  );
 }
 
 export default App;
